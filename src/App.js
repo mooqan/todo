815854c4567b053bbb8ec3b4e949c0dc62f89bb1
mooqan <districkwar@gmail.com>
2024-08-23 20:26:40 +0600

    update README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import { randomColor } from 'randomcolor';
+import Draggable from 'react-draggable';
 
 function App() {
+const [item, setItem] = useState('')
+const [items, setItems] = useState(
+  JSON.parse(localStorage.getItem('items')) || []
+)
+
+useEffect(() => {
+  localStorage.setItem('items', JSON.stringify(items))
+}, [items])
+
+const newItem = () => {
+  if(item.trim() !== '') {
+    const newItem = {
+      id: uuidv4(),
+      item,
+      color: randomColor({
+        luminosity: 'light' 
+      }),
+      defaultPos: {
+        x: 500,
+        y: -500
+      }
+    }
+    setItems((items) => [...items, newItem])
+    setItem('')
+  } else {
+    alert("Enter something...")
+    setItem('')
+  }
+}
+
+const deleteNode = (id) => {
+  setItems(items.filter((item) => item.id !== id ))
+}
+
+const updatePos = (data, index) => {
+let newArr = [...items]
+newArr[index].defaultPos = { x: data.x, y: data.y}
+setItems(newArr)
+}
+
+const keyPress = (e) => {
+  const code = e.keyCode || e.which
+  if(code === 13 ) {
+    newItem()
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <input value={item} type='text'
+        placeholder='Enter something...'
+        onChange={(e) => setItem(e.target.value) }
+        onKeyDown={(e) => keyPress(e)}
+        />
+        <button className='enter' onClick={newItem}>ENTER</button>
+      </div>
+
+{
+  items.map((item, index) => {
+    return (
+      <Draggable 
+      key={index}
+      defaultPosition={item.defaultPos}
+      onStop={(_,data) => {
+        updatePos(data, index)
+      }}
+      >
+        <div className='todo__item' style={{backgroundColor: item.color}}>
+          {`${item.item}`}
+          <button className='delete'
+          onClick={() => deleteNode(item.id)}
+          >
+            X
+          </button>
+        </div>
+
+      </Draggable>
+    )
+  })
+}
     </div>
-  );
+);
 }
 
 export default App;
